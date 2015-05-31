31ff4a408226184feb19a7068fa30595c8ce490f
Dapkin <dapkin10092@hotmail.com>
2015-05-30 23:21:34 -0500

    Initial Commit

---
+++ src/me/dapkin/drink/DrinkListeners.java
@@ -0,0 +1,222 @@
+package me.dapkin.drink;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Random;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class DrinkListeners implements Listener{
+	
+	public final DrinkPlugin ref;
+	public DrinkListeners(DrinkPlugin inst) {
+		this.ref = inst;
+	}
+	
+	@EventHandler
+	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent e) {
+		Player p = e.getPlayer();
+		Entity entity = e.getRightClicked();
+		if(e.getRightClicked() instanceof Villager) {
+			if(((LivingEntity)entity).getCustomName().equals(ref.getConfig().getString("options.villagername"))) {
+				e.setCancelled(true);
+				p.openInventory(ref.barInv);
+				p.sendMessage(ref.getConfig().getString("options.openmessage").replaceAll("(&([a-f0-9]))", "\u00A7$2"));
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e) {
+		Player player = (Player) e.getWhoClicked();
+		ItemStack clicked = e.getCurrentItem();
+		Inventory inv = e.getInventory();
+		String error = ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.DARK_RED + " You do not have sufficient funds.";
+		if(inv.getName().equals(ref.barInv.getName())) {
+			e.setCancelled(true);
+			if(clicked == null || clicked.getType() == Material.AIR) return;
+			else if(clicked.getItemMeta().getDisplayName().contains(ref.getConfig().getString("names.drink1"))) {
+				if(DrinkPlugin.economy.getBalance(player) >= ref.getConfig().getInt("price.drink1")) {
+					DrinkPlugin.economy.withdrawPlayer(player, ref.getConfig().getInt("price.drink1"));
+					player.getInventory().addItem(ref.drink1);
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink1")) +" has been taken from your account.");
+					player.closeInventory();
+				}else{
+					player.closeInventory();
+					player.sendMessage(error);
+				}
+			}else if(clicked.getItemMeta().getDisplayName().contains(ref.getConfig().getString("names.drink2"))) {
+				if(DrinkPlugin.economy.getBalance(player) >= ref.getConfig().getInt("price.drink2")) {
+					DrinkPlugin.economy.withdrawPlayer(player, ref.getConfig().getInt("price.drink2"));
+					player.getInventory().addItem(ref.drink2);
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink2")) +" has been taken from your account.");
+					player.closeInventory();
+				}else {
+					player.closeInventory();
+					player.sendMessage(error);
+				}
+			}else if(clicked.getItemMeta().getDisplayName().contains(ref.getConfig().getString("names.drink3"))) {
+				if(DrinkPlugin.economy.getBalance(player) >= ref.getConfig().getInt("price.drink3")) {
+					DrinkPlugin.economy.withdrawPlayer(player, ref.getConfig().getInt("price.drink3"));
+					player.getInventory().addItem(ref.drink3);
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink3")) +" has been taken from your account.");
+					player.closeInventory();
+				}else {
+					player.closeInventory();
+					player.sendMessage(error);
+				}
+			}else if(clicked.getItemMeta().getDisplayName().contains(ref.getConfig().getString("names.drink4"))) {
+				if(DrinkPlugin.economy.getBalance(player) >= ref.getConfig().getInt("price.drink4")) {
+					DrinkPlugin.economy.withdrawPlayer(player, ref.getConfig().getInt("price.drink4"));
+					player.getInventory().addItem(ref.drink4);
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink4")) +" has been taken from your account.");
+					player.closeInventory();
+				}else {
+					player.closeInventory();
+					player.sendMessage(error);
+				}
+			}else if(clicked.getItemMeta().getDisplayName().contains(ref.getConfig().getString("names.drink5"))) {
+				if(DrinkPlugin.economy.getBalance(player) >= ref.getConfig().getInt("price.drink5")) {
+					DrinkPlugin.economy.withdrawPlayer(player, ref.getConfig().getInt("price.drink5"));
+					player.getInventory().addItem(ref.drink5);
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink5")) +" has been taken from your account.");
+					player.closeInventory();
+				}else {
+					player.closeInventory();
+					player.sendMessage(error);
+				}
+			}else if(clicked.getItemMeta().getDisplayName().contains(ref.getConfig().getString("names.drink6"))) {
+				if(DrinkPlugin.economy.getBalance(player) >= ref.getConfig().getInt("price.drink6")) {
+					DrinkPlugin.economy.withdrawPlayer(player, ref.getConfig().getInt("price.drink6"));
+					player.getInventory().addItem(ref.drink6);
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink6")) +" has been taken from your account.");
+					player.closeInventory();
+				}else {
+					player.closeInventory();
+					player.sendMessage(error);
+				}
+			}else if(clicked.getItemMeta().getDisplayName().contains(ref.getConfig().getString("names.drink7"))) {
+				if(DrinkPlugin.economy.getBalance(player) >= ref.getConfig().getInt("price.drink7")) {
+					DrinkPlugin.economy.withdrawPlayer(player, ref.getConfig().getInt("price.drink7"));
+					player.getInventory().addItem(ref.drink7);
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink7")) +" has been taken from your account.");
+					player.closeInventory();
+				}else {
+					player.closeInventory();
+					player.sendMessage(error);
+				}
+			}else if(clicked.getType() == Material.COMPASS) {
+				player.closeInventory();
+			}else if(clicked.getType() == Material.PAPER) {
+				player.sendMessage(ChatColor.GREEN + "Balance: " + ChatColor.RED + ref.getConfig().getString("options.currencysign") + DrinkPlugin.economy.getBalance(player));
+			}
+		}
+	}
+	@EventHandler
+	public void onConsume(PlayerItemConsumeEvent e) {
+		int num = new Random().nextInt(99);
+		Player p = e.getPlayer();
+		Location loc = e.getPlayer().getLocation();
+		if(e.getItem().getType() == Material.POTION) {
+			if(e.getItem().equals(ref.drink1)) {
+				if(num < ref.getConfig().getInt("options.chance")) {
+					p.playSound(loc, Sound.LEVEL_UP, 100, 0);
+					DrinkPlugin.economy.depositPlayer(p, ref.getConfig().getInt("price.drink1") * 2);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + "You've won " + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink1") * 2) + "!");
+				}else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 20));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 20));
+				}
+			}else if(e.getItem().equals(ref.drink2)) {
+				if(num < ref.getConfig().getInt("options.chance")) {
+					p.playSound(loc, Sound.LEVEL_UP, 100, 0);
+					DrinkPlugin.economy.depositPlayer(p, ref.getConfig().getInt("price.drink2") * 2);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + "You've won " + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink2") * 2) + "!");
+				}else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 20));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 20));
+				}
+			}else if(e.getItem().equals(ref.drink3)) {
+				if(num < ref.getConfig().getInt("options.chance")) {
+					p.playSound(loc, Sound.LEVEL_UP, 100, 0);
+					DrinkPlugin.economy.depositPlayer(p, ref.getConfig().getInt("price.drink3") * 2);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + "You've won " + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink3") * 2) + "!");
+				}else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 20));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 20));
+				}
+			}else if(e.getItem().equals(ref.drink4)) {
+				if(num < ref.getConfig().getInt("options.chance")) {
+					p.playSound(loc, Sound.LEVEL_UP, 100, 0);
+					DrinkPlugin.economy.depositPlayer(p, ref.getConfig().getInt("price.drink4") * 2);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + "You've won " + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink4") * 2) + "!");
+				}else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 20));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 20));
+				}
+			}else if(e.getItem().equals(ref.drink5)) {
+				if(num < ref.getConfig().getInt("options.chance")) {
+					p.playSound(loc, Sound.LEVEL_UP, 100, 0);
+					DrinkPlugin.economy.depositPlayer(p, ref.getConfig().getInt("price.drink5") * 2);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + "You've won " + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink5") * 2) + "!");
+				}else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 20));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 20));
+				}
+			}else if(e.getItem().equals(ref.drink6)) {
+				if(num < ref.getConfig().getInt("options.chance")) {
+					p.playSound(loc, Sound.LEVEL_UP, 100, 0);
+					DrinkPlugin.economy.depositPlayer(p, ref.getConfig().getInt("price.drink6") * 2);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + "You've won " + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink6") * 2) + "!");
+				}else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 20));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 20));
+				}
+			}else if(e.getItem().equals(ref.drink7)) {
+				if(num < ref.getConfig().getInt("options.chance")) {
+					p.playSound(loc, Sound.LEVEL_UP, 100, 0);
+					DrinkPlugin.economy.depositPlayer(p, ref.getConfig().getInt("price.drink7") * 2);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.GREEN + "You've won " + ref.getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(ref.getConfig().getInt("price.drink7") * 2) + "!");
+				}else {
+					p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 20));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 20));
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onHit(EntityDamageByEntityEvent e) {
+		if(e.getDamager() instanceof Player) {
+			Player p = (Player) e.getDamager();
+			if(e.getEntity() instanceof Villager) {
+				if(e.getEntity().getCustomName().equals(ref.getConfig().getString("options.villagername"))) {
+					if(ref.getConfig().getBoolean("options.invinciblevillager")) {
+						e.setCancelled(true);
+						p.sendMessage(ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.prefix")) + " " + ChatColor.translateAlternateColorCodes('&', ref.getConfig().getString("options.cantdamage")));
+					}else {
+						return;
+					}
+				}
+			}
+		}
+	}
+	
+}
---
+++ src/me/dapkin/drink/DrinkPlugin.java
@@ -0,0 +1,150 @@
+package me.dapkin.drink;
+
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Locale;
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DrinkPlugin extends JavaPlugin implements Listener{
+
+	@Override
+	public void onEnable() {
+		initialiseConfig();
+		Bukkit.getServer().getPluginManager().registerEvents(new DrinkListeners(this), this);
+		setupEconomy();
+		setupInv();
+	}
+	@Override
+	public void onDisable() {
+		
+	}
+	public Inventory barInv = Bukkit.createInventory(null, 9, getConfig().getString("options.inventoryname"));
+	public static Economy economy = null;
+	private boolean setupEconomy() {
+		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+		if (economyProvider != null) {
+			economy = economyProvider.getProvider();
+		}
+		return (economy != null);
+	}
+	public void initialiseConfig() {
+		FileConfiguration config = getConfig();
+		
+		config.options().copyDefaults(true);
+		saveConfig();
+	}
+	public void reloadPlugin() {
+		this.reloadConfig();
+		setupInv();
+	}
+	ItemStack compass = new ItemStack(Material.COMPASS);
+	ItemStack drink1 = new ItemStack(Material.POTION);
+	ItemStack drink2 = new ItemStack(Material.POTION);
+	ItemStack drink3 = new ItemStack(Material.POTION);
+	ItemStack drink4 = new ItemStack(Material.POTION);
+	ItemStack drink5 = new ItemStack(Material.POTION);
+	ItemStack drink6 = new ItemStack(Material.POTION);
+	ItemStack drink7 = new ItemStack(Material.POTION);
+	ItemStack paper = new ItemStack(Material.PAPER);
+	public void setupInv() {
+		
+		ItemMeta icompass = compass.getItemMeta();
+		ItemMeta idrink1 = drink1.getItemMeta();
+		ItemMeta idrink2 = drink2.getItemMeta();
+		ItemMeta idrink3 = drink3.getItemMeta();
+		ItemMeta idrink4 = drink4.getItemMeta();
+		ItemMeta idrink5 = drink5.getItemMeta();
+		ItemMeta idrink6 = drink6.getItemMeta();
+		ItemMeta idrink7 = drink7.getItemMeta();
+		ItemMeta ipaper = paper.getItemMeta();
+		
+		icompass.setDisplayName(ChatColor.WHITE + getConfig().getString("options.compassname"));
+		idrink1.setDisplayName(ChatColor.WHITE + getConfig().getString("names.drink1"));
+		idrink2.setDisplayName(ChatColor.WHITE + getConfig().getString("names.drink2"));
+		idrink3.setDisplayName(ChatColor.WHITE + getConfig().getString("names.drink3"));
+		idrink4.setDisplayName(ChatColor.WHITE + getConfig().getString("names.drink4"));
+		idrink5.setDisplayName(ChatColor.WHITE + getConfig().getString("names.drink5"));
+		idrink6.setDisplayName(ChatColor.WHITE + getConfig().getString("names.drink6"));
+		idrink7.setDisplayName(ChatColor.WHITE + getConfig().getString("names.drink7"));
+		ipaper.setDisplayName(ChatColor.WHITE + getConfig().getString("options.papername"));
+		
+		icompass.setLore(Arrays.asList(ChatColor.GREEN + getConfig().getString("options.compasslore")));
+		idrink1.setLore(Arrays.asList(ChatColor.GREEN + "Price: " + getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(getConfig().getInt("price.drink1"))));
+		idrink2.setLore(Arrays.asList(ChatColor.GREEN + "Price: " + getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(getConfig().getInt("price.drink2"))));
+		idrink3.setLore(Arrays.asList(ChatColor.GREEN + "Price: " + getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(getConfig().getInt("price.drink3"))));
+		idrink4.setLore(Arrays.asList(ChatColor.GREEN + "Price: " + getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(getConfig().getInt("price.drink4"))));
+		idrink5.setLore(Arrays.asList(ChatColor.GREEN + "Price: " + getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(getConfig().getInt("price.drink5"))));
+		idrink6.setLore(Arrays.asList(ChatColor.GREEN + "Price: " + getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(getConfig().getInt("price.drink6"))));
+		idrink7.setLore(Arrays.asList(ChatColor.GREEN + "Price: " + getConfig().getString("options.currencysign") + NumberFormat.getNumberInstance(Locale.US).format(getConfig().getInt("price.drink7"))));
+		ipaper.setLore(Arrays.asList(ChatColor.GREEN + getConfig().getString("options.paperlore")));
+		
+		compass.setItemMeta(icompass);
+		drink1.setItemMeta(idrink1);
+		drink2.setItemMeta(idrink2);
+		drink3.setItemMeta(idrink3);
+		drink4.setItemMeta(idrink4);
+		drink5.setItemMeta(idrink5);
+		drink6.setItemMeta(idrink6);
+		drink7.setItemMeta(idrink7);
+		paper.setItemMeta(ipaper);
+		
+		
+		
+		barInv.setItem(0, compass);
+		barInv.setItem(1, drink1);
+		barInv.setItem(2, drink2);
+		barInv.setItem(3, drink3);
+		barInv.setItem(4, drink4);
+		barInv.setItem(5, drink5);
+		barInv.setItem(6, drink6);
+		barInv.setItem(7, drink7);
+		barInv.setItem(8, paper);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player p = (Player) sender;
+		
+		if(!(p instanceof Player)) {
+			p.sendMessage(ChatColor.RED + "You must run this command in-game!");
+		}else {	
+			if(cmd.getName().equalsIgnoreCase("villagerbar")) {
+				if(args.length == 1 && args[0].equalsIgnoreCase("spawn")) {
+					if(p.hasPermission("villagerbar.spawn") || p.isOp()) {
+						Location loc = p.getLocation();
+						p.getWorld().spawnEntity(loc, EntityType.VILLAGER).setCustomName(getConfig().getString("options.villagername".replaceAll("(&([a-f0-9]))", "\u00A7$2")));
+						p.sendMessage(getConfig().getString("options.spawnsuccess").replaceAll("(&([a-f0-9]))", "\u00A7$2"));
+					}
+				}else if(args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+					if(p.hasPermission("villagerbar.reload") || p.isOp()) {
+						p.sendMessage(getConfig().getString("options.reloadsuccess").replaceAll("(&([a-f0-9]))", "\u00A7$2"));
+						reloadPlugin();
+					}
+				}
+			}else if(cmd.getName().equalsIgnoreCase("bar")) {
+				if(p.hasPermission("villagerbar.virtual") && p.isOp()) {
+					p.openInventory(barInv);
+					p.sendMessage(getConfig().getString("options.openmessage").replaceAll("(&([a-f0-9]))", "\u00A7$2"));
+				}
+			}
+		}
+		return true;
+	}
+
+}
